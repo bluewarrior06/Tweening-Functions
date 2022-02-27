607a226000d678cd9797aa27c77c79d8f5d2ed26
joey merryfield <maroon12champ@gmail.com>
2022-02-26 21:23:23 -0400

    Simple Tweening Algorithm, but its commented :O

--- Tweening Functions/Tween.cpp
+++ Tweening Functions/Tween.cpp
@@ -1,16 +1,29 @@
 #include "Tween.h"
 
+/*
+	How it works
+	
+	t - time
+	sv - starting value
+	ev - ending value
+
+	interpolates from sv to ev with t
+	t only goes from 0 - 1, but you can go further if you want
+*/
+//basically just a lerp function
 float Tween::LinearTween(float t, float sv, float ev)
 {
 	return ((ev - sv) * t) + sv;
 
 }
 
+//smoothly transitions in
 float Tween::CubicTweenIn(float t, float sv, float ev)
 {
 	return ((ev - sv) * t * t * t) + sv;
 }
 
+//smoothly transitions out
 float Tween::CubicTweenOut(float t, float sv, float ev)
 {
 	float _t = t;
@@ -18,8 +31,10 @@ float Tween::CubicTweenOut(float t, float sv, float ev)
 	return ((ev - sv) * (t * t * t + 1)) + sv;
 }
 
+//smoothly transitions in, than out
 float Tween::CubicTweenInOut(float t, float sv, float ev)
 {
+	//starting
 	float a = CubicTweenIn(t * 2, sv, Tween::LinearTween(0.5, sv, ev));
 	float b = CubicTweenOut((t * 2) - 1, Tween::LinearTween(0.5, sv, ev), ev);
 	if (t < 0.5)
@@ -28,6 +43,7 @@ float Tween::CubicTweenInOut(float t, float sv, float ev)
 	}
 	return b;
 }
+//smoothly transitions out, than back in
 float Tween::CubicTweenOutIn(float t, float sv, float ev)
 {
 	float a = Tween::CubicTweenOut(t * 2, sv, Tween::LinearTween(0.5, sv, ev));
--- Tweening Functions/Tween.h
+++ Tweening Functions/Tween.h
@@ -1,12 +1,28 @@
 #pragma once
 
+/*
+	How it works
+
+	t - time
+	sv - starting value
+	ev - ending value
+
+	interpolates from sv to ev with t
+	t only goes from 0 - 1, but you can go further if you want
+*/
+
 namespace Tween
 {
+	//basically just a lerp function
 	float LinearTween(float t, float sv, float ev);
 
+	//smoothly transitions in
 	float CubicTweenIn(float t, float sv, float ev);
+	//smoothly transitions out
 	float CubicTweenOut(float t, float sv, float ev);
 
+	//smoothly transitions in, than out
 	float CubicTweenInOut(float t, float sv, float ev);
+	//smoothly transitions out, than back in
 	float CubicTweenOutIn(float t, float sv, float ev);
 }
